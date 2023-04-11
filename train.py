acea0adbd9b4a8a111e127cddce44854e210c8df
aalsaedi265 <aalsaedi265@gmail.com>
2023-04-11 12:53:16 -0500

    training completed

--- train.py
+++ train.py
@@ -20,61 +20,87 @@ for intent in intents['intents']:
         w = tokenize(pattern)
         all_words.extend(w)#extend spreads the arr
         xy.append((w,tag))
-ignore_words=["?","!",".",","]
-all_words=[stem(w) for w in all_words if w not in ignore_words]
+        
+ignore_words = ['?', '.', '!']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+# remove duplicates and sort
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-# print(all_words)
-# print('xy: ',xy)
-x_train = []
-y_train =[] #will be an array of 1.0 & 0
-for( pattern_sentece,tag) in xy:
-    bag = bag_of_words(pattern_sentece, all_words)
-    x_train.append(bag)
-    
+
+print(len(xy), "patterns")
+print(len(tags), "tags:", tags)
+print(len(all_words), "unique stemmed words:", all_words)
+
+# create training data
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    # X: bag of words for each pattern_sentence
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+    # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
     label = tags.index(tag)
-    y_train.append(label)#array of indexs
-# print(x_train)
-x_train= np.array(x_train)
-y_train=np.array(y_train)
+    y_train.append(label)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+# Hyper-parameters 
+num_epochs = 1000
+batch_size = 8
+learning_rate = 0.001
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(tags)
 
 class ChatDataset(Dataset):
+
     def __init__(self):
-        self.n_samples = len(x_train)
-        self.x_data = x_train
-        self.y_data= y_train
-    #dataset[idx]
-    def __getitem__(self,idx):
-        return self.x_data[idx], self.y_data[idx]
-    
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    # support indexing such that dataset[i] can be used to get i-th sample
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    # we can call len(dataset) to return the size
     def __len__(self):
         return self.n_samples
 
-batch_size =8
-hidden_size=8
-output_size= len(tags)
-input_size = len(x_train[0])
-learning_rate = 0.001
-num_epochs = 1000
-# print(input_size, len(all_words))
-# print(output_size, tags)
-    
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset,batch_size=batch_size, shuffle=True ,num_workers=0)#experiemnt with 2 or 0
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
+# Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+# Train the model
 for epoch in range(num_epochs):
-    for(words, labels) in train_loader:
+    for (words, labels) in train_loader:
         words = words.to(device)
-        labels = labels.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
         
+        # Forward pass
         outputs = model(words)
+        # if y would be one-hot, we must apply
+        # labels = torch.max(labels, 1)[1]
         loss = criterion(outputs, labels)
         
+        # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+
+print(f'final loss: {loss.item():.4f}')
